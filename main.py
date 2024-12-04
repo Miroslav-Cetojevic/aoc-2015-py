0a425251d7bba978b3cd6c2ca736e3a5023a2b2f
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2024-12-04 23:19:48 +0100

    Renamed some variables
    Separate the all_days dict into an all_days list and a journal dict.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from config import *
 from solutions.day1 import Day1
 from solutions.day2 import Day2
 
-
 class Result(NamedTuple):
     value: Any
     milliseconds: float
@@ -16,10 +15,11 @@ def avg_runtime(func, n):
     return Result(value, ((end - start) / n) / 1_000_000)
 
 if __name__ == '__main__':
-    all_days = {i: func for i, func in enumerate([Day1, Day2], start=1)}
+    all_days = [Day1, Day2]
+    journal = {i: func for i, func in enumerate(all_days, start=1)}
 
     index = 2
-    chosen_day = all_days[index](join('inputs', f'day{index}-input'))
+    chosen_day = journal[index](join('inputs', f'day{index}-input'))
     print('Part 1:', avg_runtime(chosen_day.part1, 10_000))
     print('Part 2:', avg_runtime(chosen_day.part2, 10_000))
 
