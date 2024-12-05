33fa765dc5d4d442a06f9ddc385d63538b859bc6
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2024-12-05 12:52:59 +0100

    Update main and config file

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+import hashlib
+import itertools
+
+import numpy as np
+
+from collections import defaultdict
+from numbers import Number
 from time import perf_counter_ns
 from typing import NamedTuple, Any
 
 from os.path import join
-
-import numpy as np
 
 from solutions.day import Day
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from config import *
 from solutions.day1 import Day1
 from solutions.day2 import Day2
+from solutions.day3 import Day3
+from solutions.day4 import Day4
+from solutions.day5 import Day5
+
 
 class Result(NamedTuple):
-    value: Any
+    value: Number
     milliseconds: float
 
 def avg_runtime(func, n):
@@ -15,11 +19,11 @@ def avg_runtime(func, n):
     return Result(value, ((end - start) / n) / 1_000_000)
 
 if __name__ == '__main__':
-    all_days = [Day1, Day2]
+    all_days = [Day1, Day2, Day3, Day4, Day5]
     journal = {i: func for i, func in enumerate(all_days, start=1)}
 
-    index = 2
+    index = 5
     chosen_day = journal[index](join('inputs', f'day{index}-input'))
-    print('Part 1:', avg_runtime(chosen_day.part1, 10_000))
-    print('Part 2:', avg_runtime(chosen_day.part2, 10_000))
+    print('Part 1:', avg_runtime(chosen_day.part1, 1_000))
+    print('Part 2:', avg_runtime(chosen_day.part2, 1_000))
 
